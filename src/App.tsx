efc1ffb0f642b19d7a0094bef127e95d1a8475ed
tdkryk <tdkryk@icloud.com>
2024-07-09 04:43:02 +0900

    todo-component-added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,9 +37,9 @@ function App() {
         </div>
       </form>
       <ul className="divide-y divide-gray-200 px-4">
-        {/* {data?.map((todo) => (
-          <Todo key={todo.id} todo={todo} />
-        ))} */}
+        <Todo key={1} />
+		<Todo key={2} />
+		<Todo key={3} />
       </ul>
     </div>
   )
